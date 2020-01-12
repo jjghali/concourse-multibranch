fe4bd4936fe0fae27a8ca568ebde16e81cd0909e
Joshua Ghali <joshua.ghali@gologic.ca>
2020-01-12 17:20:40 -0500

    make commander work

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-import "./polyfills";
+#!/usr/bin/env node
+
+import './polyfills';
+
+import * as boxen from 'boxen';
+import * as chalk from 'chalk';
+import * as commander from 'commander';
+
+commander.version('1.0.0').description('Concourse Multibranch generator');
+
+commander.command('hello-world').description('Helloworld').action(() => {
+	console.log(chalk.yellow('=========*** Hello world! ***=========='));
+});
+
+if (!process.argv.slice(2).length /* || !/[arudl]/.test(process.argv.slice(2))*/) {
+	commander.outputHelp();
+	process.exit();
+}
+commander.parse(process.argv);
