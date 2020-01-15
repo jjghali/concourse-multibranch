08ccfce8fae446cc4ef9f728aba1534cd5214a45
Joshua Ghali <joshua.ghali@gologic.ca>
2020-01-15 10:18:40 -0500

    work

--- test/jobtransformer.test.ts
+++ test/jobtransformer.test.ts
@@ -6,7 +6,6 @@ describe("JobTransformer", () => {
         it("should return the new pipeline", () => {
             let jbt: JobTransformer = new JobTransformer('./concourse-test.yml', 'template');
 
-
             jbt.generatePipeline(['branche1', 'branche2'])
         });
     });
